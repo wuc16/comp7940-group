c0ec18f77651046bddd968e6edde95673523e7dd
Chen Wu <chenwu@ChendeMacBook-Pro.local>
2023-04-10 19:32:30 +0800

    some modification

--- chatbot.py
+++ chatbot.py
@@ -2,7 +2,7 @@
 import openai
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
-import configparser
+# import configparser
 import os
 import logging
 import firebase_admin
@@ -14,8 +14,8 @@ import random
 def main():
     # Load your token and create an Updater for your Bot
 
-    config = configparser.ConfigParser()
-    config.read('config.ini')
+    # config = configparser.ConfigParser()
+    # config.read('config.ini')
 
     # link to telegram chatbot
     # updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
